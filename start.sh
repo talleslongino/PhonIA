e439043c0226f11ad7194a012cbee77b93de68d3
Talles Longino <talleslongino@hotmail.com>
2025-05-22 03:00:16 -0300

    Atualiza arquivo de inicialização da aplicação no server

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-uvicorn api.main:app --host 0.0.0.0 --port 10000
+source /home/talles/analisevoz/phonia_v0/phonia/venv/bin/activate
+exec uvicorn api.main:app --host 0.0.0.0 --port 8000
